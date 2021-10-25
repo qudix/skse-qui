ac9fe5a0e19246ca393ec5b1586b01f82f561c0b
Qudix <17361645+Qudix@users.noreply.github.com>
2021-10-25 17:37:51 -0500

    Minor script stuff

--- src/c++/Script/Script.h
+++ src/c++/Script/Script.h
@@ -2,6 +2,7 @@
 
 namespace Script
 {
+	using InternalVM = RE::BSScript::Internal::VirtualMachine;
 	using ObjectPtr = RE::BSTSmartPointer<RE::BSScript::Object>;
 	using ArrayPtr = RE::BSTSmartPointer<RE::BSScript::Array>;
 	using CallbackPtr = RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>;
@@ -9,14 +10,14 @@ namespace Script
 
 	inline RE::VMHandle GetHandle(RE::TESForm* a_form)
 	{
-		auto vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+		auto vm = InternalVM::GetSingleton();
 		auto policy = vm->GetObjectHandlePolicy();
 		return policy->GetHandleForObject(a_form->GetFormType(), a_form);
 	}
 
 	inline ObjectPtr GetObject(RE::TESForm* a_form, const char* a_class, bool a_create = false)
 	{
-		auto vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+		auto vm = InternalVM::GetSingleton();
 		auto handle = GetHandle(a_form);
 
 		ObjectPtr object = nullptr;
@@ -49,8 +50,16 @@ namespace Script
 	template <class... Args>
 	inline bool DispatchMethodCall(ObjectPtr a_obj, RE::BSFixedString a_fnName, CallbackPtr a_callback, Args&&... a_args)
 	{
-		auto vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+		auto vm = InternalVM::GetSingleton();
 		auto args = RE::MakeFunctionArguments(std::forward<Args>(a_args)...);
 		return vm->DispatchMethodCall(a_obj, a_fnName, args, a_callback);
+	}
+
+	template <class... Args>
+	inline bool DispatchStaticCall(RE::BSFixedString a_class, RE::BSFixedString a_fnName, CallbackPtr a_callback, Args&&... a_args)
+	{
+		auto vm = InternalVM::GetSingleton();
+		auto args = RE::MakeFunctionArguments(std::forward<Args>(a_args)...);
+		return vm->DispatchStaticCall(a_class, a_fnName, args, a_callback);
 	}
 }
