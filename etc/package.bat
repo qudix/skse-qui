eb90a8316ef4702d41b91b6cccaad4c0193111ce
Qudix <17361645+Qudix@users.noreply.github.com>
2021-10-25 22:05:19 -0500

    Package script

---
+++ etc/package.bat
@@ -0,0 +1,5 @@
+robocopy "%~dp0../build/Release/" "%~dp0package/SKSE/Plugins/" "*.dll"
+robocopy "%~dp0../res/" "%~dp0package/SKSE/Plugins/" "*.toml"
+robocopy "%~dp0../src/swf/" "%~dp0package/Interface/" "*.swf"
+tar -a --cd "package" -c -f "QUI-0.1.0.zip" "*.*"
+rmdir /q /s package
