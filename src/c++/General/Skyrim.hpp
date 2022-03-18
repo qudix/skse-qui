4eab03307eefdf6797f8da68dd40b217a06d1212
Qudix <17361645+Qudix@users.noreply.github.com>
2022-03-17 22:38:24 -0500

    Use RELOCATION_ID macro

--- src/c++/General/Skyrim.hpp
+++ src/c++/General/Skyrim.hpp
@@ -10,11 +10,7 @@ namespace General::Skyrim
 	inline bool ActivateRef(RE::TESObjectREFR* a_self, RE::TESObjectREFR* a_activator, uint8_t a_arg2, RE::TESBoundObject* a_object, int32_t a_count, bool a_defaultProcessingOnly)
 	{
 		using func_t = decltype(&ActivateRef);
-#ifdef SKYRIM_SUPPORT_AE
-		REL::Relocation<func_t> func{ REL::ID(19796) };
-#else
-		REL::Relocation<func_t> func{ REL::ID(19369) };
-#endif
+		REL::Relocation<func_t> func{ RELOCATION_ID(19369, 19796) };
 		return func(a_self, a_activator, a_arg2, a_object, a_count, a_defaultProcessingOnly);
 	}
 }
